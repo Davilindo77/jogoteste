454af642630a32785df14794c4e213383420cb38
Davilindo77 <davi.sousa24@aluno.educa.go.gov.br>
2026-05-12 20:20:30 -0300

    camera.js

--- systems/camera.js
+++ systems/camera.js
@@ -1,1 +1,15 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js' export function setupCamera() { const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ) camera.position.set(0, 5, 5) return camera } 
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js&#39;
+
+export function setupCamera() {
+
+const camera = new THREE.PerspectiveCamera(
+75,
+window.innerWidth / window.innerHeight,
+0.1,
+1000
+)
+
+camera.position.set(0, 5, 5)
+
+return camera
+}
